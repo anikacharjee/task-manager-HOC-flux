69f39f96f9df062eb487ee5cc32c5e2cf1d8cb72
Anik Acharjee <anikacharjee@users.noreply.github.com>
2025-10-30 10:12:24 +0530

    Add files via upload

---
+++ src/reducers/taskReducer.js
@@ -0,0 +1,16 @@
+import { ADD_TASK, TOGGLE_TASK, DELETE_TASK } from '../flux/actionTypes';
+
+export const taskReducer = (state, action) => {
+  switch (action.type) {
+    case ADD_TASK:
+      return [...state, { id: Date.now(), text: action.payload, done: false }];
+    case TOGGLE_TASK:
+      return state.map(task =>
+        task.id === action.payload ? { ...task, done: !task.done } : task
+      );
+    case DELETE_TASK:
+      return state.filter(task => task.id !== action.payload);
+    default:
+      return state;
+  }
+};
